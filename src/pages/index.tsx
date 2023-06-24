92b657a588974ef358ecd93c8ffa7a21738ac092
Lucho Web <luchoweb.dev@gmail.com>
2023-06-24 10:16:56 -0500

    Dashboard progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
 export { default as HomePage } from "./public/home";
 export { default as ImagesPage } from "./public/images";
 export { default as LoginPage } from "./public/login";
+export { default as DashboardPage } from "./private/dashboard";
--- src/pages/private/dashboard.tsx
+++ src/pages/private/dashboard.tsx
@@ -3,7 +3,9 @@ import Layout from "../layout";
 const DashboardPage = () => {
   return (
     <Layout>
+     <div className="container pt-4 pb-4">
       <p>Dashboard</p>
+     </div>
     </Layout>
   );
 }
