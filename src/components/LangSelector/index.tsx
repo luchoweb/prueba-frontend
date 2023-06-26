443f919f4f55c52b82a5bb3982c8868357ff9f0e
Lucho Web <luchoweb.dev@gmail.com>
2023-06-26 13:34:42 -0500

    Redux and lang selector added

---
+++ src/components/LangSelector/index.tsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeLangAction } from "../../redux/slices/langSlice";
+
+import "./styles.scss";
+
+const LangSelector = () => {
+  const lang = useSelector((state: any) => state.lang.lang);
+  const dispatch = useDispatch();
+
+  const handleChangeLang = ( newLang: string ) => {
+    dispatch(changeLangAction(newLang))
+  }
+
+  return (
+    <select
+      className="lang-selector form-select ms-2"
+      onChange={(event) => handleChangeLang(event.target.value)}
+    >
+      <option value="es" selected={lang === "es"}>
+        ES
+      </option>
+      <option value="en" selected={lang === "en"}>
+        EN
+      </option>
+    </select>
+  );
+};
+
+export default LangSelector;
